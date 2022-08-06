3f522897c656bf8b33d648ea0d7f9ca4e2edbd91
Jaswinder Singh <jas19winder@gmail.com>
2022-08-06 16:09:35 -0400

    shop module routing and imported dependencies for ng prime

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component'
+import { CheckoutComponent } from './checkout/checkout.component'
+import { FeaturedComponent } from './featured/featured.component'
 const routes: Routes = [
   {
     path: '',
+    component: FeaturedComponent,
+  },
+  {
+    path: 'cart',
     component: CartComponent,
   },
   {
+    path: 'checkout',
+    component: CheckoutComponent,
+  },
+  {
     path: '**',
-    component: CartComponent,
+    component: FeaturedComponent,
   },
 ];
 
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,17 +1,50 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ShopRoutingModule } from './shop-routing.module';
-import { CartComponent } from './cart/cart.component';
 
+import { CartComponent } from './cart/cart.component';
+import { FeaturedComponent } from './featured/featured.component';
+
+//prime ng
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ToastModule} from 'primeng/toast';
+import {BadgeModule} from 'primeng/badge';
+
+//local service
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { CheckoutService } from './services/checkout.service';
+import { CartService } from './services/cart.service';
+
+//local components
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
-  declarations: [
-    CartComponent
+  declarations: 
+  [
+    CartComponent,
+    FeaturedComponent,
+    CheckoutComponent
   ],
   imports: [
     CommonModule,
-    ShopRoutingModule
-  ]
+    ShopRoutingModule,
+    CardModule,
+    ButtonModule,
+    SplitButtonModule,
+    ConfirmDialogModule,
+    ToastModule,
+    BadgeModule
+  ],
+  providers: 
+  [
+    ConfirmationService,
+    MessageService,
+    CheckoutService,
+    CartService
+  ],
 })
 export class ShopModule { }
