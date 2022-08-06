2e2adb76a78f65516108e46201964150dc311598
Jaswinder Singh <jas19winder@gmail.com>
2022-08-06 16:08:27 -0400

    shop module services

---
+++ src/app/shop/services/cart.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject ,of , Observable, Subject} from 'rxjs';
+
+import { Product } from '../../products/models/product.model'
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService 
+{
+  public cartItemList : any =[]
+  public productList = new BehaviorSubject<any>([]);
+  public search = new BehaviorSubject<string>("");
+  count = 0;
+  simpleObservable = new Subject();
+  simpleObservable$ = this.simpleObservable.asObservable();
+  constructor() { }
+
+  getProducts()
+  {
+    return this.productList.asObservable();
+  }
+
+  setProduct(product : any)
+  {
+    this.cartItemList.push(...product);
+    this.productList.next(product);
+  }
+
+  addtoCart(product : Product)
+  {
+    this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+    this.simpleObservable.next(this.count)
+    // console.log(this.productList)
+  }
+  
+  getTotalPrice() : number
+  {
+    let grandTotal = 0;
+    this.cartItemList.map((a:any)=>
+    {
+      grandTotal += a.price;
+    })
+    return grandTotal;
+  }
+
+  removeCartItem(product: Product)
+  {
+    this.cartItemList.map((a:any, index:any)=>
+    {
+      if(product.id=== a.id)
+      {
+        this.cartItemList.splice(index,1);
+      }
+    })
+
+    this.productList.next(this.cartItemList);
+    this.simpleObservable.next(this.count)
+    console.log(this.productList)
+
+  }
+
+  removeAllCart()
+  {
+    this.cartItemList = []
+    this.productList.next(this.cartItemList);
+    this.simpleObservable.next(this.count)
+  }
+}
---
+++ src/app/shop/services/checkout.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService 
+{
+  constructor(private http: HttpClient) { }
+
+  makePayment(stripeToken: any): Observable<any>
+  {
+    const url = "http://localhost:5000/checkout/"
+ 
+    return this.http.post<any>(environment.apiBaseUrl+'/stripe',{token:stripeToken})
+  }
+}
