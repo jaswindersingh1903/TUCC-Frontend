271d57747ac567b94e453d2a8acfc4f1529748d8
Jaswinder Singh <jas19winder@gmail.com>
2022-08-14 19:56:52 -0400

    routes added in sidebar --issue fixed

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -11,32 +11,59 @@ export class SidebarComponent implements OnInit {
   constructor(private cartService : CartService) { }
     public totalCartItems:any=0;
     display
-    items: { label: string, icon: string }[] = [
+    items: { label: string, icon: string,routerLink:string }[] = 
+    [
       {
         label: 'Home',
         icon: 'pi pi-fw pi-home',
-        // routerLink: 'core/home',
+        routerLink: 'core/home',
+        
       },
       {
         label: 'About',
-        icon: 'pi pi-fw pi-info-circle'
+        icon: 'pi pi-fw pi-info-circle',
+        routerLink: 'core/about'
       },
       {
         label: 'Catalog',
-        icon: 'pi pi-fw pi-book'
+        icon: 'pi pi-fw pi-book',
+        routerLink: 'catalog'
       },
       {
         label: 'Products',
-        icon: 'pi pi-fw pi-video'
+        icon: 'pi pi-fw pi-video',
+        routerLink: 'products'
+          
       },
       {
-        label: 'Shop',
-        icon: 'pi pi-shopping-bag'
+          label: 'Shop',
+          icon: 'pi pi-shopping-bag',
+          routerLink: 'shop',
       },
-      {
-        label: 'Cart',
-        icon: 'pi pi-shopping-cart'
-      }
+      /* {
+          label: 'Cart',
+          icon: 'pi pi-shopping-cart',
+          routerLink: 'shop/cart'
+        
+      }, */
+      /* {
+          label: '',
+          icon: 'pi pi-briefcase',
+          items: [
+            {label: 'Shop', icon: 'pi pi-shopping-bag',routerLink: 'shop'},
+            {label: 'Cart', icon: 'pi pi-shopping-cart',routerLink: 'shop/cart'}
+        ]
+          // routerLink: 'shop'
+        
+      }, */
+      /* {
+          label: '',
+          icon: 'pi pi-fw pi-key',
+          items: [
+              {label: 'Login', icon: 'pi  pi-users',routerLink: 'auth/login'},
+              {label: 'Signup', icon: 'pi pi-user-plus',routerLink: 'auth/register'}
+          ]
+      } */
     ];
   ngOnInit()
   {
