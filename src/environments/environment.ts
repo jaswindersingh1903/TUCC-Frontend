a8e30cae6ab7f3acb5bd30f62ead2f2a21b786ea
Jaswinder Singh <jas19winder@gmail.com>
2022-08-14 19:09:00 -0400

    env file with stripe key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://127.0.0.1:8000/api/V1',
+  stripeKey: 'pk_test_51LTC43BkXJmv3iKTsJrPPG3TEblHodI2HZLP2TmoNf5YsSHqob5zkgODjMsTb279z0gFiLVsaL5DWMTDooKkBT5L00PuE7lzm8',
   // apiBaseUrl:'1',
   // API :apiBaseUrl+'/'+apiBaseUrl,
 };
