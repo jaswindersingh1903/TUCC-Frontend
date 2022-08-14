9987885a4eb361420964a77d59359f2fd4e0c0a0
Jaswinder Singh <jas19winder@gmail.com>
2022-08-14 19:02:52 -0400

    added sidebar using ng sidebar routing issue

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService} from '../../../shop/services/cart.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
-  display
-  ngOnInit(): void {
-  }
+  constructor(private cartService : CartService) { }
+    public totalCartItems:any=0;
+    display
+    items: { label: string, icon: string }[] = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        // routerLink: 'core/home',
+      },
+      {
+        label: 'About',
+        icon: 'pi pi-fw pi-info-circle'
+      },
+      {
+        label: 'Catalog',
+        icon: 'pi pi-fw pi-book'
+      },
+      {
+        label: 'Products',
+        icon: 'pi pi-fw pi-video'
+      },
+      {
+        label: 'Shop',
+        icon: 'pi pi-shopping-bag'
+      },
+      {
+        label: 'Cart',
+        icon: 'pi pi-shopping-cart'
+      }
+    ];
+  ngOnInit()
+  {
+          this.getCartCount();
 
+  }
+  getCartCount()
+  {
+    this.cartService.getProducts()
+    .subscribe(
+      res=>
+      {
+        this.totalCartItems = res.length;
+        console.log('header component',this.totalCartItems)
+
+        // this.products = res;
+        // this.grandTotal = this.cartService.getTotalPrice();
+      }
+    )
+  }
 }
