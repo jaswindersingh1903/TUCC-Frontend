a2d2b7126276490a8db3547d9ba531f7d73c4d22
Jaswinder Singh <jas19winder@gmail.com>
2022-08-16 23:26:02 -0400

    New card design for featured component

--- src/app/shop/featured/featured.component.ts
+++ src/app/shop/featured/featured.component.ts
@@ -18,8 +18,64 @@ import {  Product } from '../../products/models/product.model';
 })
 export class FeaturedComponent implements OnInit {
   items: MenuItem[];
-  productsList: Product[]=[];
+  // productsList: Product[]=[];
 
+  productsList: { product_name: string, price: any, image: any, desc: string }[] = [
+    {
+      product_name: 'You Are Here: Help!',
+      price: '$150.00',
+      image: 'https://catalog.churchx.ca/images/EDGE1030Coursethumbnail.png',
+      desc: "Now that you and your leadership team have gone through Signpost #1 - You Are Here: Help!, are you wondering about the next step in your community of faith's pathway to change? Use this Signpost to help you and your community of faith get ready to begin a journey of curiosity and learning that will steer it toward a new future.!"
+    },
+    {
+      product_name: 'Getting Ready for the Journey',
+      price: '$150.00',
+      image: 'https://catalog.churchx.ca/images/EDGEsignpost2.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://catalog.churchx.ca/images/SustainabilityCohortthumbnail.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://www.primefaces.org/primeblocks-ng/assets/images/blocks/ecommerce/productlist/product-list-2-4.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://www.primefaces.org/primeblocks-ng/assets/images/blocks/ecommerce/productlist/product-list-2-1.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://www.primefaces.org/primeblocks-ng/assets/images/blocks/ecommerce/productlist/product-list-2-2.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://www.primefaces.org/primeblocks-ng/assets/images/blocks/ecommerce/productlist/product-list-2-3.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://www.primefaces.org/primeblocks-ng/assets/images/blocks/ecommerce/productlist/product-list-2-4.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    },
+    {
+      product_name: 'Product Name',
+      price: '$150.00',
+      image: 'https://www.primefaces.org/primeblocks-ng/assets/images/blocks/ecommerce/productlist/product-list-2-4.png',
+      desc: "This course, the first in EDGE's Signpost Series, is designed to help communities of faith in crisis mode take the first steps on a change journey. Participants will learn to assess and contextualize their current situation and to build a leadership team to address issues. You'll see that a way back to hope is possible!"
+    }
+  ];
   //
  
   constructor(
@@ -32,9 +88,8 @@ export class FeaturedComponent implements OnInit {
 
   ngOnInit(): void 
   {
-    this.getFeaturedProducts();
+    // this.getFeaturedProducts();
     this.invokeButton();
-    
   }
 
   public invokeButton()
