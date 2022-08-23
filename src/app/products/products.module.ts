a3a32b2cc55d8d2a2b0f7b5e5971dbfb92683bbf
Jaswinder Singh <jas19winder@gmail.com>
2022-08-23 01:52:23 -0400

    product module with material design

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -8,17 +8,50 @@ import { EditComponent } from './components/edit/edit.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+//angular material 
+// import { MatSliderModule } from '@angular/material/slider';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table'  
+import {MatChipsModule} from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {FileUploadModule} from 'primeng/fileupload';
+import {MultiSelectModule} from 'primeng/multiselect';
+import { IndexComponent } from './components/index/index.component';
+
 @NgModule({
   declarations: [
     AddComponent,
     ViewComponent,
     EditComponent,
+    IndexComponent,
 
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    // FormsModule, 
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    FileUploadModule,
+    MultiSelectModule,
+    MatIconModule,
+    MatTableModule,
+    MatChipsModule,
+    MatNativeDateModule
   ],
   providers:[
     ProductService
