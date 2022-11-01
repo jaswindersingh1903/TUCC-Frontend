21bea8ca72d95439449133080e35fc74fc43342c
Jaswinder Singh <jas19winder@gmail.com>
2022-11-01 02:30:19 -0400

    product model

---
+++ src/app/products/models/NewProduct.model.ts
@@ -0,0 +1,35 @@
+export interface Product {
+    Product Name?:any;
+    Product Sub-Title?: number;
+    Product Code?: string;
+    Date Edited?: string;
+    Active Item?: any;
+    Out of Stock?: any;
+
+    Show in Catalog / Product Listings
+    Featured
+    Description
+    Description2
+    Short Description
+    Keywords
+
+    Meta Title
+    Meta Description
+    Allow Invoice:
+    Thumbnail URL
+    Heading Image URL:
+    Large Image URL:	
+
+
+    Inventory
+    Visibility Start Date/Time
+    Visibility End Date/Time:	
+    Purchasable Start Date/Time:	
+    Purchasable End Date/Time
+    Prices:
+    
+    Product Finance Code:
+    Product Owner Emails
+    Related Products:
+    Categories
+  }
