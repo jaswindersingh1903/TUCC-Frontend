d04fefa5c624cdacf0c611a50c30f634be7e3629
Jaswinder Singh <jas19winder@gmail.com>
2022-11-01 02:30:55 -0400

    env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://127.0.0.1:8000/api/V1',
+  stripeKey: 'pk_test_51LTC43BkXJmv3iKTsJrPPG3TEblHodI2HZLP2TmoNf5YsSHqob5zkgODjMsTb279z0gFiLVsaL5DWMTDooKkBT5L00PuE7lzm8',
+  // apiBaseUrl:'1',
 };
